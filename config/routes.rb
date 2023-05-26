823feb64f68e958178e41138a15f1652db900c18
Pouyan Soltanpour <soltanpour.p@gmail.com>
2023-05-26 15:19:24 +0100

    updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  root 'lists#index'
+  root to: 'lists#index'
 
-  resources :lists, only: [:index, :show, :new, :create] do
+  resources :lists, only: [:show, :new, :create] do
     resources :bookmarks, only: [:new, :create]
   end
 
